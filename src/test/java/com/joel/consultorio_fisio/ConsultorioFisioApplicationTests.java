7f40c0c37cd380ace36621d811eec0c720623192
Joel Júnior <joel.fmjr@gmail.com>
2025-11-29 09:20:45 -0300

    feat: Initial commit

---
+++ src/test/java/com/joel/consultorio_fisio/ConsultorioFisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.consultorio_fisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioFisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
